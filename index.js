991f4e230c798f7572f777104d30def6854d53fb
Patrick Blaine <apatrickstar@icloud.com>
2016-12-14 04:31:35 -0800

    added ternary operator for the last 4 and error handling for card verification

--- index.js
+++ index.js
@@ -472,7 +472,7 @@ app.post('/smsReceived', function(req, res) {
                                     twilio.sendMessage({
                                         to: latestMessage.from, // Any number Twilio can deliver to
                                         from: AllMyPPL.PHONE_NUMBER, // A number you bought from Twilio and can use for outbound communication
-                                        body: "The last 4 digits of your active payment method are " + cards[0].last4 + ".\n\nTo set a new payment method, type USERNAME PASSWORD payment set CARD_NUMBER EXP_MONTH EXP_YEAR CVV."
+                                        body: "The last 4 digits of your active payment method are " + cards[0].last4 ? cards[0].last4 : "ERROR"  + ".\n\nTo set a new payment method, type USERNAME PASSWORD payment set CARD_NUMBER EXP_MONTH EXP_YEAR CVV."
                                     }, function(err, responseData) { //this function is executed when a response is received from Twilio
                                         if (!err) {
                                             console.log("Successfully sent sms to " + latestMessage.from + ". Body: " + responseData);
@@ -548,7 +548,7 @@ app.post('/smsReceived', function(req, res) {
 
                                                             }).then(function(source) {
                               */
-/*
+
                                 var customerUpdatePromise = new Parse.Promise();
 
                                 if (resultData.user.get('customerId')) {
@@ -560,7 +560,7 @@ app.post('/smsReceived', function(req, res) {
                                     }, function(err, customer) {
                                       // asynchronously called
                                       if (err) {
-                                        customerUpdatePromise.reject(new Parse.Error(Parse.Error.SCRIPT_FAILED,AllMyPPL.STRIPE_ERROR_MESSAGE));
+                                        customerUpdatePromise.reject(new Parse.Error(Parse.Error.SCRIPT_FAILED,AllMyPPL.STRIPE_ERROR_MESSAGE + "  Tell support that you had an error of this type: " + err.type));
                                       } else {
                                         customerUpdatePromise.resolve(customer);
                                       }
@@ -573,9 +573,8 @@ app.post('/smsReceived', function(req, res) {
                                 return customerUpdatePromise;
 
                               }).then(function(customer) {
-*/
+
                                 console.log("Card verified successfully.");
-                                console.log("card number " + sourceToken.number);
 
                                 twilio.sendMessage({
                                     to: latestMessage.from, // Any number Twilio can deliver to
@@ -592,7 +591,11 @@ app.post('/smsReceived', function(req, res) {
                                 resultPromise.resolve();
                               }, function (error) {
                                 console.log("Card verification failed.");
-                                resultPromise.reject(new Parse.Error(error.code,error.message));
+                                if (error.type) {
+                                  resultPromise.reject(new Parse.Error(error.type,error.message));
+                                } else {
+                                  resultPromise.reject(new Parse.Error(error.code,error.message));
+                                }
                               });
 
                         } else if (resultData.paymentCommand == "delete") {
