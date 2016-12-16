d4a24c5d6bdd56e6871ea74591eb5d7203cfbd25
Patrick Blaine <apatrickstar@icloud.com>
2016-12-16 15:20:05 -0800

    made being dirty a condition of all tests

--- cloud/main.js
+++ cloud/main.js
@@ -25,11 +25,12 @@ Parse.Cloud.beforeSave(Parse.User, (req, res) => {
   console.log('user dirty keys: ',user.dirtyKeys());
 
   var emailDirty;
-  for (let key in user.dirtyKeys()) {if (key == 'email') {emailDirty = true; break;}}
+  var usernameDirty;
+  for (let key in user.dirtyKeys()) {if (key == 'email') {emailDirty = true; break;} if (key == 'username') {usernameDirty = true; break;}}
 
-  if (!user.get('username') || user.get('username') == '') {res.error('A username must be provided.');}
-  else if (user.get('username') != user.get('username').toLowerCase()) {res.error('A username must consist only of lower case letters.');}
-  else if (!user.get('email') || user.get('email') == '' || !validateEmail(user.get('email'))) {res.error('A valid email address must be provided.');}
+  if (usernameDirty && !user.get('username') || user.get('username') == '') {res.error('A username must be provided.');}
+  else if (usernameDirty && user.get('username') != user.get('username').toLowerCase()) {res.error('A username must consist only of lower case letters.');}
+  else if (emailDirty && !user.get('email') || user.get('email') == '' || !validateEmail(user.get('email'))) {res.error('A valid email address must be provided.');}
   else if (emailDirty && user.get('email') == obj.get('email')) {res.error('Attempting to update a user\'s email address with the value it already has is not permitted.');}
   else { res.success();}
 });
