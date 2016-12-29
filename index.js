a455b6a4b29af241f964b764606969c6ccdbc1b9
Patrick Blaine <apatrickstar@icloud.com>
2016-12-28 16:05:17 -0800

    added contactId query for after contact has been unset from isEmergencyContact

--- index.js
+++ index.js
@@ -223,8 +223,17 @@ app.post('/smsReceived', function(req, res) {
                     emergencyContact.unset("isEmergencyContact");
                     emergencyContact.save(null, {sessionToken:commandData.user.getSessionToken()}).then(function(saved){
                       console.log("isEmergencyContact unset on object "+JSON.stringify(saved));
-                      resultData.result = saved;
-                      commandPromise.resolve(resultData);
+
+                      var contactIdQuery = new Parse.Query(Contact);
+                      contactIdQuery.get(commandData.contactId,{sessionToken:commandData.user.getSessionToken()}).then(function(contact){
+                        console.log("contactIdQuery.get happened");
+                        if (!contact) {console.log(commandData.contactId+" did not return a contact."); commandPromise.reject(new Parse.Error(Parse.Error.OBJECT_NOT_FOUND,commandData.contactId+" did not return a contact."));}
+                        else {console.log(contact.id); contact.set("isEmergencyContact",true); contact.save(null, {sessionToken:commandData.user.getSessionToken()}).then(function(saved){console.log("saved object ",saved.toJSON()); resultData.result = saved; commandPromise.resolve(resultData);},function(error){console.log("saving object failed."); commandPromise.reject(new Parse.Error(error.code,error.message));});}
+                      },function(error) {
+                        console.log("contactIdQuery.get failed, "+error.code+" : "+error.message+" "+commandData.contactId);
+                        commandPromise.reject(new Parse.Error(error.code,error.message));
+                      });
+
                     },function (error) {
                       console.log("save of object failed, "+error.code+" : "+error.message+" "+commandData.contactId);
                       commandPromise.reject(new Parse.Error(error.code,error.message));
@@ -235,6 +244,7 @@ app.post('/smsReceived', function(req, res) {
                   commandPromise.reject(new Parse.Error(error.code,error.message));
                 });
             } else {
+                console.log("looking up current emergencyContact info...");
                 emergencyContactQuery.equalTo("isEmergencyContact",true);
                 emergencyContactQuery.first({sessionToken:commandData.user.getSessionToken()}).then(function(emergencyContact) {
                   if (!emergencyContact) { console.error("No emergencyContact found.")}
