9c2e0ce14fecd48d4052ea021b97feaaf33976a7
Patrick Blaine <apatrickstar@icloud.com>
2017-04-01 01:03:52 -0700

    now instead of looping back through MyPPL

--- routes/voiceCall.js
+++ routes/voiceCall.js
@@ -398,8 +398,8 @@ router.post('/MyPPL/:numeral', twilio.webhook({validate: false}), function(reque
   var twiml = new twilio.TwimlResponse();
   twiml.say("my people");
   var numeral = parseInt(request.params.numeral);
-  if (numeral >= 10) { twiml.say("End of My people.  Looping back through.  If you're hearing this message right after starting the list, you haven't designated anyone as My people in the All my people eye oh ess app.",{voice:'alice'}); twiml.redirect('/voice/menu/0'); response.type('text/xml'); response.send(twiml.toString()); return;}
-  else if (numeral <= 0) { twiml.say("Listing my people, if you know the selection you want you can enter it any time during silence.",{voice:'alice'}); response.type('text/xml'); response.send(twiml.toString());}
+  if (numeral >= 10) { twiml.say("End of My people. Returning to the main menu.",{voice:'alice'}); twiml.redirect('/voice/menu/0'); response.type('text/xml'); response.send(twiml.toString()); return;}
+  else if (numeral == 0) { twiml.say("Listing my people.",{voice:'alice'});twiml.redirect('/voice/MyPPL/1'); response.type('text/xml'); response.send(twiml.toString()); return;}
   var query = new Parse.Query("Contact");
   query.equalTo("numeral",numeral+"");
   query.first({sessionToken:user.getSessionToken()}).then(function(contact){
